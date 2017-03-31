b25e904d1b53b6197709fe7f3d393dd089280b21
Andreas Josefsson <andreas@barnensbibliotek.se>
2017-03-31 16:43:54 +0200

    20170331 startat med loghandler

---
+++ kulturkatalogenAPI_v1/info/arrangemangInfo.vb
@@ -0,0 +1,30 @@
+﻿Public Class arrangemangInfo
+    'TODO lägg till alla parametrar som behövs och tillkommer i arrangemangstabellen
+
+    Private _arrid As Integer
+    Private _rubrik As String
+    Public Sub New()
+        _arrid = 0
+        _rubrik = ""
+    End Sub
+    Public Property Arrid() As Integer
+        Get
+            Return _arrid
+        End Get
+        Set(ByVal value As Integer)
+            _arrid = value
+        End Set
+    End Property
+
+
+    Public Property Rubrik() As String
+        Get
+            Return _rubrik
+        End Get
+        Set(ByVal value As String)
+            _rubrik = value
+        End Set
+    End Property
+
+
+End Class
---
+++ kulturkatalogenAPI_v1/info/logInfo.vb
@@ -0,0 +1,72 @@
+﻿Public Class logInfo
+
+    Private _logid As Integer
+    Private _logtyp As Integer
+    Private _datum As Date
+    Private _arrid As Integer
+    Private _status As String
+    Private _arrangemang As List(Of arrangemangInfo)
+
+    Public Sub New()
+        _logid = 0
+        _logtyp = 0
+        _datum = Date.Now
+        _arrid = 0
+        _status = ""
+        _arrangemang = New List(Of arrangemangInfo)
+    End Sub
+    Public Property logid() As Integer
+        Get
+            Return _logid
+        End Get
+        Set(ByVal value As Integer)
+            _logid = value
+        End Set
+    End Property
+
+
+    Public Property logtyp() As Integer
+        Get
+            Return _logtyp
+        End Get
+        Set(ByVal value As Integer)
+            _logtyp = value
+        End Set
+    End Property
+
+    Public Property Datum() As Date
+        Get
+            Return _datum
+        End Get
+        Set(ByVal value As Date)
+            _datum = value
+        End Set
+    End Property
+
+    Public Property Arrid() As Integer
+        Get
+            Return _arrid
+        End Get
+        Set(ByVal value As Integer)
+            _arrid = value
+        End Set
+    End Property
+
+    Public Property Status() As String
+        Get
+            Return _status
+        End Get
+        Set(ByVal value As String)
+            _status = value
+        End Set
+    End Property
+
+    Public Property LogArrangemang() As List(Of arrangemangInfo)
+        Get
+            Return _arrangemang
+        End Get
+        Set(ByVal value As List(Of arrangemangInfo))
+            _arrangemang = value
+        End Set
+    End Property
+End Class
